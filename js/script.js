aa8046896111618bad622ed0d220fc76c4b2680c
majczel23000 <michal.razny23@gmail.com>
2020-11-01 19:33:25 +0100

    feat(about): about section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-    setTimeout(() => {
-        $('.loader__container').addClass('loader-fade');
-    }, 500);
-    setTimeout(() => {
-        $('.loader__container').css('display', 'none');
-    }, 1500);
+    // setTimeout(() => {
+    //     $('.loader__container').addClass('loader-fade');
+    // }, 500);
+    // setTimeout(() => {
+    //     $('.loader__container').css('display', 'none');
+    // }, 1500);
    
     $('nav ul li a:not(:only-child)').click(function(e) {
         $(this).siblings('.nav-dropdown').toggle();
