c7df859c9962217006009df2241d672a74e24b74
majczel23000 <michal.razny23@gmail.com>
2020-11-03 22:03:19 +0100

    feat(services,pricing,portfolio): services, pricing, portfolio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(() => {
-    // setTimeout(() => {
-    //     $('.loader__container').addClass('loader-fade');
-    // }, 500);
-    // setTimeout(() => {
-    //     $('.loader__container').css('display', 'none');
-    // }, 1500);
+
+    // Smooth scrolling to anhor
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 25
+        }, 500);
+    });
+
+    setTimeout(() => {
+        $('.loader__container').addClass('loader-fade');
+    }, 500);
+    setTimeout(() => {
+        $('.loader__container').css('display', 'none');
+    }, 1500);
    
     $('nav ul li a:not(:only-child)').click(function(e) {
         $(this).siblings('.nav-dropdown').toggle();
@@ -24,3 +33,18 @@ $(document).ready(() => {
         this.classList.toggle('active');
     });
 });
+
+function openPortfolioModal(number) {
+    const modal = document.getElementById("portfolioModal" + number);
+    modal.style.display = "block";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function closePortfolioModal(modalID) {
+    const modal = document.getElementById(modalID);
+    if (modal) modal.style.display = "none";
+}
